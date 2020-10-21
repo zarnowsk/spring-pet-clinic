45fc279a380a2f73b4ee749c419b7d954ed3ecad
mzarnowskED <59625249+mzarnowskED@users.noreply.github.com>
2020-10-21 14:27:13 -0400

    Added Speciality Map impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/mich/springframework/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package mich.springframework.springpetclinic.services;
+
+import mich.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
