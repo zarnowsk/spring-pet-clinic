4078bbcd01228a51e8bb7e200f7af468e7b9f1e8
mzarnowskED <59625249+mzarnowskED@users.noreply.github.com>
2020-10-21 13:49:48 -0400

    Added PetType service. Closes #29.

---
+++ pet-clinic-data/src/main/java/mich/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package mich.springframework.springpetclinic.services;
+
+import mich.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
