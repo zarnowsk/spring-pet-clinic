0eb3c0dacd35046b577af14211004c2234a7af4f
mzarnowskED <59625249+mzarnowskED@users.noreply.github.com>
2020-10-22 11:41:19 -0400

    Updated Vet to use JPA mappings. Closes #37.

--- pet-clinic-data/src/main/java/mich/springframework/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/mich/springframework/springpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package mich.springframework.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
