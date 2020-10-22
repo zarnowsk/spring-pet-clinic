66c45156b25d124dadb1c341dc2a6c27847529ef
mzarnowskED <59625249+mzarnowskED@users.noreply.github.com>
2020-10-22 14:58:35 -0400

    Added Spring data JPA service for Vets. Closes #41.

---
+++ pet-clinic-data/src/main/java/mich/springframework/springpetclinic/services/springdatajpa/VetJpaService.java
@@ -0,0 +1,48 @@
+package mich.springframework.springpetclinic.services.springdatajpa;
+
+import mich.springframework.springpetclinic.model.Vet;
+import mich.springframework.springpetclinic.repositories.VetRepository;
+import mich.springframework.springpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return  vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
